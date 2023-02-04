313bc3f00948795ca6d9ffdf8ba919d95e016ac8
Anastasiia Fursyk <anastasianauk44@gmail.com>
2023-02-04 20:28:11 +0200

    query filter, centuries filter were added

--- src/components/People/PeopleFilters.tsx
+++ src/components/People/PeopleFilters.tsx
@@ -1,4 +1,33 @@
+import classNames from 'classnames';
+import { Link, useSearchParams } from 'react-router-dom';
+
 export const PeopleFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries') || [];
+
+  function updateSearch(params: { [key: string]: string | string[] | null }) {
+    Object.entries(params).forEach(([key, value]) => {
+      if (value === null) {
+        searchParams.delete(key);
+      } else if (Array.isArray(value)) {
+        searchParams.delete(key);
+
+        value.forEach(part => {
+          searchParams.append(key, part);
+        });
+      } else {
+        searchParams.set(key, value);
+      }
+    });
+
+    setSearchParams(searchParams);
+  }
+
+  const onQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateSearch({ query: event.target.value || null });
+  };
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
@@ -16,6 +45,8 @@ export const PeopleFilters = () => {
             type="search"
             className="input"
             placeholder="Search"
+            value={query}
+            onChange={onQueryChange}
           />
 
           <span className="icon is-left">
@@ -27,66 +58,65 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
-            >
-              16
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
-            >
-              17
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
-            >
-              18
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
+            {[16, 17, 18, 19, 20].map(century => (
+              <Link
+                key={century}
+                data-cy="century"
+                className={classNames(
+                  'button',
+                  'mr-1',
+                  { 'is-info': centuries.includes(century.toString()) },
+                )}
+                to={`..?centuries=${century.toString()}`}
+                onClick={() => {
+                  updateSearch({
+                    centuries: centuries.includes(century.toString())
+                      ? centuries.filter(cent => +cent !== century)
+                      : [...centuries, century.toString()],
+                  });
+                }}
+              >
+                {century.toString()}
+              </Link>
+            ))}
           </div>
 
           <div className="level-right ml-4">
-            <a
+            <Link
               data-cy="centuryALL"
               className="button is-success is-outlined"
-              href="#/people"
+              to="./"
+              onClick={() => {
+                if (centuries.length < 5) {
+                  updateSearch({
+                    centuries: ['16', '17', '18', '19', '20'],
+                  });
+                } else {
+                  updateSearch({
+                    centuries: null,
+                  });
+                }
+              }}
             >
               All
-            </a>
+            </Link>
           </div>
         </div>
       </div>
 
       <div className="panel-block">
-        <a
+        <Link
           className="button is-link is-outlined is-fullwidth"
-          href="#/people"
+          to="#/people"
+          onClick={() => {
+            updateSearch({
+              query: null,
+              centuries: null,
+            });
+          }}
         >
           Reset all filters
-        </a>
+        </Link>
       </div>
     </nav>
   );
--- src/components/People/PersonLink.tsx
+++ src/components/People/PersonLink.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import className from 'classnames';
 import { Person } from '../../types';
 
@@ -7,13 +7,17 @@ type Props = {
   person: Person,
 };
 
-export const PersonLink: React.FC<Props> = memo(({ person }) => (
-  <Link
-    to={`../${person.slug}`}
-    className={className(
-      { 'has-text-danger': person.sex === 'f' },
-    )}
-  >
-    {person.name}
-  </Link>
-));
+export const PersonLink: React.FC<Props> = memo(({ person }) => {
+  const location = useLocation();
+
+  return (
+    <Link
+      to={`../${person.slug}/${location.search}`}
+      className={className(
+        { 'has-text-danger': person.sex === 'f' },
+      )}
+    >
+      {person.name}
+    </Link>
+  );
+});
